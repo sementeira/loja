c349cf7c18f3bba78b43eb29a9d0603abb77dc97
Estevo U. C. Castro <euccastro@gmail.com>
2020-12-29 23:36:10 +0100

    add barebones web app

--- src/clj/loja/system.clj
+++ src/clj/loja/system.clj
@@ -1,12 +1,17 @@
 (ns loja.system
   (:require
-   [loja.crux :as crux]))
+   [loja.crux :as crux]
+   [loja.web :as web]))
 
-(defn start [{:keys [crux-dir]}]
-  (let [node (crux/crux-node crux-dir)]
-    {:crux-node node}))
+(defn start [{:keys [crux-dir http-port]}]
+  (let [crux-node (crux/crux-node crux-dir)
+        handler (web/handler crux-node)
+        http-server (web/start-server http-port handler)]
+    {:crux-node crux-node
+     :http-server http-server}))
 
-(defn stop [{:keys [crux-node]}]
+(defn stop [{:keys [crux-node http-server]}]
+  (web/stop-server http-server)
   (crux/close crux-node))
 
 (comment
--- src/clj/loja/web.clj
+++ src/clj/loja/web.clj
@@ -1,3 +1,42 @@
 (ns loja.web
   (:require
-   [ring.adapter.jetty :as jetty]))
+   [clojure.pprint :refer [pprint]]
+   [hiccup.core :refer [html]]
+   [hiccup.page :refer [doctype]]
+   [ring.adapter.jetty :as jetty]
+   [ring.util.http-response :refer [content-type ok]])
+  (:import [org.eclipse.jetty.server Server]))
+
+(defn- html5-ok
+  ([title body]
+   (html5-ok title [] body))
+  ([title head body]
+   (-> (html
+        (doctype :html5)
+        `[:html
+          [:head
+           [:meta {:charset "UTF-8"}]
+           [:meta {:name "viewport"
+                   :content "width=device-width, initial-scale=1.0"}]
+           [:title ~title]
+           ~@head]
+          [:body
+           ~@body]])
+       ok
+       (content-type "text/html; charset=utf-8"))))
+
+(defn handle [crux-node req]
+  (html5-ok "Echo" [[:div "Prova"]
+                    [:pre (with-out-str (pprint req))]]))
+
+(defn handler [crux-node]
+  (fn [req]
+    (handle crux-node req)))
+
+(defn start-server [http-port http-handler]
+  (let [port (or http-port 62000)]
+    (jetty/run-jetty http-handler {:port port :join? false})))
+
+(defn stop-server [server]
+  (.stop ^Server server))
+
