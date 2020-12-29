59f76715b37a061dfff10e473c5b5b9fdc5fb31d
Estevo U. C. Castro <euccastro@gmail.com>
2020-12-29 15:57:18 +0100

    test/fix add-shopkeeper function

--- src/clj/loja/ops.clj
+++ src/clj/loja/ops.clj
@@ -10,7 +10,12 @@ Para criar a tua senha clica no seguinte endereço:
 
 ")
 
-(defn add-shopkeeper [system display-name email]
+(defn add-shopkeeper [{:keys [callback-host
+                              callback-path
+                              password]
+                       :as system}
+                      display-name
+                      email]
   (if-let [eid (db-sk/add-shopkeeper system display-name email)]
     (email/send-email
      system
@@ -18,10 +23,22 @@ Para criar a tua senha clica no seguinte endereço:
       :subject "Invitaçom para atender a loja da Semente"
       :body (str
              msg
-             "http://localhost:9500/senha/"
+             callback-host
+             callback-path
              (crypto/urlsafe-encrypt
-              system
               [:set-password
                {:id eid
-                :t (System/currentTimeMillis)}]))})
+                :t (System/currentTimeMillis)}]
+              password))})
     (throw (ex-info "could not transact shopkeeper" {}))))
+
+(comment
+  (require '[loja.config :as config])
+  (require '[crux.api :as crux])
+  (def node (crux/start-node {}))
+  (add-shopkeeper (assoc (config/load "dev")
+                         :crux-node node)
+                  "Manolo Peres"
+                  "euccastro@gmail.com")
+  (.close node)
+  )
